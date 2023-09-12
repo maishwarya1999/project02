3748345237a705026c19375bf060eaaf4c271ac7
maishwarya1999 <maishwarya0401@gmail.com>
2023-09-11 20:04:50 -0400

    method

---
+++ src/learning.java
@@ -0,0 +1,10 @@
+public class learning {
+    static void myMethod() {
+            System.out.println("calling method");
+        }
+        public static void main(String[] args) {
+            myMethod();
+        }
+    }
+
+
