c19625a1ef691607db61f7c220d4956511dbda69
maishwarya1999 <maishwarya0401@gmail.com>
2023-09-11 20:06:49 -0400

    Sum using method

---
+++ src/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+        static int myMethod(int x, int y) {
+            return x + y;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(myMethod(5, 3));
+        }
+    }
+
